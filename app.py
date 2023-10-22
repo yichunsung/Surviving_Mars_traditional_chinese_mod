080869f376774ec04ca5d7cf676084d85b4a6af6
YiChun Sung <ycsung.r@gmail.com>
2023-10-23 02:58:50 +0800

    fix: removed test code

--- app.py
+++ app.py
@@ -11,9 +11,3 @@ with open('./source_data/Schinese/CurrentLanguage/future.csv', newline='') as fu
 cc = OpenCC('s2twp')
 text = '所有穿梭机都可以在此处停靠、补充燃料，用来进行仓库之间远距离资源运输任务，以及穹顶间居民再分配的任务。'
 print(cc.convert(text))
-
-ironmen = numpy.array([46, 8, 11, 11, 4, 56]) # 將 list 透過 numpy 的 array 方法進行轉換
-print(ironmen) # 看看 ironmen 的外觀
-print(type(ironmen)) # 看看 ironmen 的資料結構
-articles = ironmen * 30
-print(articles)
